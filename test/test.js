702390f0db093b5cc0d52346391101aa36362081
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-11-28 10:34:53 +0000

    first test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,22 @@
+'use strict'
 var test = require('tape')
-var lib = require('../')
+var commandLineCommands = require('../')
 
-test('first', function(t){
-
+test('first', function (t) {
+  const commands = [
+    {
+      name: 'eat',
+      definitions: [ { name: 'food' } ]
+    },
+    {
+      name: 'sleep',
+      definitions: [ { name: 'hours' } ]
+    }
+  ]
+  const cli = commandLineCommands(commands)
+  let options = cli.parse([ 'eat', '--food', 'peas' ])
+  t.deepEqual(options, { eat: { food: 'peas' } })
+  options = cli.parse([ 'sleep', '--hours', '2' ])
+  t.deepEqual(options, { sleep: { hours: '2' } })
+  t.end()
 })

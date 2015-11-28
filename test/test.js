d7e23ecfd1a852fe429514294857bc1a6139e1f5
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-11-28 10:54:08 +0000

    different output format.. more tests

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 var test = require('tape')
 var commandLineCommands = require('../')
 
-test('first', function (t) {
+test('parse: simple', function (t) {
   const commands = [
     {
       name: 'eat',
@@ -14,9 +14,35 @@ test('first', function (t) {
     }
   ]
   const cli = commandLineCommands(commands)
-  let options = cli.parse([ 'eat', '--food', 'peas' ])
-  t.deepEqual(options, { eat: { food: 'peas' } })
-  options = cli.parse([ 'sleep', '--hours', '2' ])
-  t.deepEqual(options, { sleep: { hours: '2' } })
+  let command = cli.parse([ 'eat', '--food', 'peas' ])
+  t.deepEqual(command, {
+    name: 'eat',
+    options: { food: 'peas' }
+  })
+  command = cli.parse([ 'sleep', '--hours', '2' ])
+  t.deepEqual(command, {
+    name: 'sleep',
+    options: { hours: '2' }
+  })
+  t.end()
+})
+
+test('parse: no definitions', function (t) {
+  const commands = [ { name: 'eat' } ]
+  const cli = commandLineCommands(commands)
+  let command = cli.parse([ 'eat' ])
+  t.deepEqual(command, {
+    name: 'eat',
+    options: { }
+  })
+  t.end()
+})
+
+test('parse: no definitions, but options passed', function (t) {
+  const commands = [ { name: 'eat' } ]
+  const cli = commandLineCommands(commands)
+  t.throws(() => {
+    let command = cli.parse([ 'eat', '--food', 'peas' ])
+  })
   t.end()
 })

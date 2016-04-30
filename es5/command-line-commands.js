44d60a2bd788b55d1de804e7fd60b09d0267be1e
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-04-30 13:35:36 +0100

    update es5

--- es5/command-line-commands.js
+++ es5/command-line-commands.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -9,7 +9,7 @@ var arrayify = require('array-back');
 
 module.exports = factory;
 
-var CommandLineCommands = (function () {
+var CommandLineCommands = function () {
   function CommandLineCommands(commands) {
     _classCallCheck(this, CommandLineCommands);
 
@@ -38,10 +38,24 @@ var CommandLineCommands = (function () {
       }
       return output;
     }
+  }, {
+    key: 'getUsage',
+    value: function getUsage(commandName, options) {
+      var output = '';
+      var commandDefinition = this.commands.find(function (c) {
+        return c.name === commandName;
+      });
+      if (commandDefinition) {
+        var cli = commandLineArgs(commandDefinition.definitions);
+        output = cli.getUsage(options);
+      }
+
+      return output;
+    }
   }]);
 
   return CommandLineCommands;
-})();
+}();
 
 function factory(commands) {
   return new CommandLineCommands(commands);

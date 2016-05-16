850e1b6600e4e47f43a7bfbd8f72c786e4f02b98
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-05-16 20:26:23 +0100

    readme

--- example/synopsis.js
+++ example/synopsis.js
@@ -4,4 +4,4 @@ const validCommands = [ null, 'clean', 'update', 'install' ]
 const { command, argv } = commandLineCommands.parse(validCommands)
 
 console.log('command: %s', command)
-console.log('argv: %s', JSON.stringify(argv))
+console.log('argv:    %s', JSON.stringify(argv))
